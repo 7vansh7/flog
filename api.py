2799e696b9ead7784e5fa049cc8bf13996e1dfd5
vansh aggarwal <vanshaggarwal@vanshs-MacBook-Pro.local>
2024-10-25 23:03:09 +0530

    .

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ def read_root():
 @app.get("/wallet_info/{Key}")
 def wallet_info(Key: str):
     # add the logic to retrieve wallet info using public key from the DB
-    return {"item_id": item_id, "query": q}
+    return {"message":"your holdings are - "}
 
 @app.post("/create_wallet/")
 def create_new_wallet():
