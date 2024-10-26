82657312f30068c455dc45b3ba41354cbc9cffd6
vansh aggarwal <vanshaggarwal@vanshs-MacBook-Pro.local>
2024-10-27 00:20:22 +0530

    .

--- wallet_db.py
+++ wallet_db.py
@@ -1,18 +1,35 @@
 import sqlite3
+import hashlib
 
 
 conn = sqlite3.connect('./walletDB')
 c = conn.cursor()
 c.execute(""" CREATE TABLE IF NOT EXISTS wallets (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                public_key TEXT,
-                private_key TEXT,
+                id INTEGER UNIQUE AUTOINCREMENT,
+                public_key TEXT UNIQUE,
+                private_key TEXT PRIMARY KEY,
                 holdings TEXT,
                 timestamp TEXT
              )""")
 
-def add_data():
-	pass 
+def create_user():
 
-def get_data():
-	pass
+	public_key = ''
+	private_key = ''
+	c.execute(""" INSERT INTO wallets (public_key, private_key) 
+		VALUES (?,?)""",(public_key, private_key))
+	conn.commit()
+
+def get_user_holdings(public_key):
+
+	c.execute(""" SELECT holdings FROM wallets WHERE public_key = ? """,(public_key))
+	holdings = c.fetchall()
+	return holdings
+
+def update_user_holdings(private_key, change):
+
+	c.execute(""" UPDATE wallets SET holdings = holdings || ?
+	 WHERE private_key = ? """,(holdings,private_key)
+
+
+
