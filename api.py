c8b836521e0ca9c047ae07307444b6d03429a4e9
vansh aggarwal <vanshaggarwal@vanshs-MacBook-Pro.local>
2024-11-01 12:06:40 +0530

    .

--- api.py
+++ api.py
@@ -2,25 +2,32 @@ from fastapi import FastAPI
 import uvicorn
 import sys 
 sys.path.append('./')
-from wallet_db import get_data,add_data
+from wallet_db import create_user,get_user_holdings,add_user_holdings
 
 app = FastAPI()
 
+# replace the get methods with the post methods
 
 @app.get("/")
 def read_root():
-    return {"message": "Fuck you, this is void"}
+    return {"message": "Fuck you, this is a void"}
 
-@app.get("/wallet_info/{Key}")
-def wallet_info(Key: str):
-    # add the logic to retrieve wallet info using public key from the DB
-    return {"message":"your holdings are - "}
+@app.get("/wallet_info/{Private_key}")
+def wallet_info(Private_key: str):
+    holdings = get_user_holdings(private_key=Private_key)[0][0]
+    return {"message": holdings}
 
-@app.post("/create_wallet/")
+@app.get("/create_wallet/")
 def create_new_wallet():
-        # add the logic to create private and public key, also store the info in a DB
-    return {"message":"wallet created successfully", "private_key":"","public_key":""}
+    private_key, public_key = create_user()
+    return {"message":"wallet created successfully, please store these keys as they won't be shown again ",
+             "private_key": private_key, "public_key": public_key}
 
+@app.get("/wallet_add/{Private_key}/{Change}")
+def add_to_holdings(Private_key,Change):
+    add_user_holdings(Private_key,Change)
+    return {"message":f"added {Change}"}
 
 if __name__ == "__main__":
     uvicorn.run('api:app', host="127.0.0.1", port=8000, reload=True)
+
--- wallet_db.py
+++ wallet_db.py
@@ -1,35 +1,54 @@
 import sqlite3
 import hashlib
+import os 
+import ecdsa
 
 
-conn = sqlite3.connect('./walletDB')
+conn = sqlite3.connect('./wallets.db', check_same_thread=False)
 c = conn.cursor()
-c.execute(""" CREATE TABLE IF NOT EXISTS wallets (
-                id INTEGER UNIQUE AUTOINCREMENT,
-                public_key TEXT UNIQUE,
-                private_key TEXT PRIMARY KEY,
-                holdings TEXT,
-                timestamp TEXT
-             )""")
+
+
+c.execute(""" 
+CREATE TABLE IF NOT EXISTS wallets (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    public_key TEXT UNIQUE,
+    private_key TEXT UNIQUE,
+    holdings TEXT,
+    timestamp TEXT
+)""")
 
 def create_user():
+    private_key = os.urandom(32)
+    signing_key = ecdsa.SigningKey.from_string(private_key, curve=ecdsa.SECP256k1)
+    verifying_key = signing_key.verifying_key
+    public_key = b'\04' + verifying_key.to_string()
+    c.execute("INSERT INTO wallets (public_key, private_key) VALUES (?, ?)", (public_key.hex(), private_key.hex()))
+    print(private_key.hex() ,"\n\n", public_key.hex())
+    conn.commit()
+    return private_key.hex(),public_key.hex()
 
-	public_key = ''
-	private_key = ''
-	c.execute(""" INSERT INTO wallets (public_key, private_key) 
-		VALUES (?,?)""",(public_key, private_key))
+def get_user_holdings(private_key):
+
+    c.execute("SELECT holdings FROM wallets WHERE private_key = ?", (private_key,))
+    holdings = c.fetchall()
+    return holdings
+
+def add_user_holdings(private_key, change):
+           
+	c.execute("""UPDATE wallets SET holdings = COALESCE(holdings, '') || ? WHERE private_key = ?""", (change, private_key))
 	conn.commit()
-
-def get_user_holdings(public_key):
-
-	c.execute(""" SELECT holdings FROM wallets WHERE public_key = ? """,(public_key))
-	holdings = c.fetchall()
-	return holdings
-
-def update_user_holdings(private_key, change):
-
-	c.execute(""" UPDATE wallets SET holdings = holdings || ?
-	 WHERE private_key = ? """,(holdings,private_key)
+     
 
 
 
+# priv, pub = create_user()
+# add_user_holdings('3c7871139c08108affca26f46664d915299ebafc64c2667801c66393fb1e9fc6',"car")
+holdings = get_user_holdings(private_key='3c7871139c08108affca26f46664d915299ebafc64c2667801c66393fb1e9fc6')
+print(holdings[0][0])
+
+# c.execute("SELECT * FROM wallets WHERE private_key = ?", (priv,))
+# print(c.fetchall())
+
+# 3c7871139c08108affca26f46664d915299ebafc64c2667801c66393fb1e9fc6 
+
+#  04d8205ebf5aa6edbec154fe364cb3f91ec3ae521a27d8447f14853d75db0af4eb974a5b06cce25048a5f41dc4dda5e2e7d6135294fb0bbfaea1b8e42ad5a25546
